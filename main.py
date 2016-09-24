88bd8a25474001eda21099c60f1629b05cbfe522
Danbot <dankovacek@gmail.com>
2016-09-24 04:01:14 -0700

    add placeholder images, add signup form

--- main.py
+++ main.py
@@ -23,26 +23,34 @@ from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
+from webapp2_extras import sessions
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(template_dir),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+# webapp2 session configuration
+config = {}
+
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'some-secret-key',
+}
+
 # [END imports]
 # os.path.dirname(__file__)
 #jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 #    autoescape = True)
 
-DEFAULT_GUESTBOOK_NAME = 'Zero Waste City Challenge'
+#DEFAULT_GUESTBOOK_NAME = 'Team Name'
 
 # We set a parent key on the 'Audits' to ensure that they are all
 # in the same entity group. Queries across the single entity group
 # will be consistent. However, the write rate should be limited to
 # ~1/second.
 
-# add handler for easier write calls
-
+# add handlers for easier write calls
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -54,8 +62,24 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    #get a session store for this request
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
 
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+        try:
+            #Dispatch the request
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            #save all sessions
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+
+def guestbook_key(guestbook_name='temp'):
     """Constructs a Datastore key for a Guestbook entity.
 
     We use guestbook_name as the key.
@@ -66,12 +90,18 @@ def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
 # [START user]
 class User(ndb.Model):
     """Sub model for representing a user."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
+    #identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(required=True)
+    team = ndb.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    #TODO: link Team to User
+    team_img = ndb.StringProperty(required=True)
 
     @classmethod
     def by_name(cls, name):
-        u = User.all().filter('username =', name).get()
+        u = User.all().filter('name =', name).get()
         return u
 
     @classmethod
@@ -89,43 +119,80 @@ class Audit(ndb.Model):
 class Team(ndb.Model):
     admin = ndb.StructuredProperty(User)
     team_name = ndb.StringProperty(required=True)
-    team_type = ndb.StringProperty(required=True)
+    organization_type = ndb.StringProperty(required=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
+    team_img = ndb.StringProperty(required = False)
+
+    @classmethod
+    def by_team_name(cls, name):
+        t = Team.all().filter('team_name =', name).get()
+        return t
+
+    @classmethod
+    def by_user_email(cls, email):
+        u = User.all().filter('email =', email).get()
+        team_name = u.team
+        t = by_team_name(team_name)
+        return t
+
 # [END audit]
 
 
 # [START main_page]
 class MainPage(Handler):
-
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        audits_query = Audit.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Audit.date)
-        audits = audits_query.fetch(10)
+        user_email = self.session.get('email')
 
-        user = users.get_current_user()
+        print '#######user email = %s' % user_email
 
-        if user:
-            url = users.create_logout_url(self.request.uri)
+        # guestbook_name = self.request.get('guestbook_name',
+        #                                   DEFAULT_GUESTBOOK_NAME)
+        # audits_query = Audit.query(
+        #     ancestor=guestbook_key(guestbook_name)).order(-Audit.date)
+        # audits = audits_query.fetch(10)
+
+        # user = users.get_current_user()
+
+        #TODO: query audit entities
+        audits = ''
+        if user_email:
+            #url = users.create_logout_url(self.request.uri)
+            url = '/logout'
             url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
 
-        print '$$$$$$$$$$$4 url_linktext = %s' % url_linktext
+            #retrieve the User object from database
+            u = User.by_email(user_email)
+        else:
+            #url = users.create_login_url(self.request.uri)
+            url = '/login'
+            url_linktext = 'Login'
+            u = None
 
         self.render('index.html')
-        self.render('header.html', url=url, url_linktext=url_linktext,
-            guestbook_name=guestbook_name, user=user)
-        self.render('content.html', user=user, audits=audits,
-            guestbook_name=urllib.quote_plus(guestbook_name), url=url)
+        self.render('header.html', user=u, url_linktext=url_linktext, url=url)
+        self.render('content.html', user=u, audits=audits, url=url)
 # [END main_page]
+
+# TODO: implement create Team Entity
+# def CreateTeam(self, email, team_name):
+#     t = Team.by_email(email)
+#     # check if user already exists.
+
+#     if u:
+#         msg = 'User already registered.'
+#         self.redirect('/')
+#     else:
+#         new_user = User(username=session_obj['username'],
+#             email=session_obj['email'], picture=session_obj['picture'],
+#             provider=session_obj['provider'],
+#             provider_id=session_obj['provider_id'])
+#         new_user.put()
+#         user = getUserInfo(self, session_obj['email'])
+#         #return user
 
 
 # [START guestbook]
 class Guestbook(webapp2.RequestHandler):
-
     def post(self):
         # We set the same parent key on the 'Audit' to ensure each
         # Audit is in the same entity group. Queries across the
@@ -150,11 +217,98 @@ class Guestbook(webapp2.RequestHandler):
 
 class Login(Handler):
     def get(self):
-        pass
+        self.render('login.html', user='', login_out='login')
+        self.render('header.html', team_name='')
+
+    def post(self):
+        email = self.request.get('email')
+        pwd = self.request.get('password')
+        email_error, password_error = None, None
+
+        # user = User.by_email(email)
+        # if user:
+        #     user_error = "User account already exists"
+
+        #u = User.by_email(email)
+
+        if not email:
+            email_error = "Please use a valid e-mail address."
+
+        if not pwd:
+            password_error = "Please use only alphanumeric characters."
+
+        if (email_error or password_error):
+            self.render("login.html", email=email,
+                email_error=email_error, password_error=password_error)
+            self.render('header.html', user='', url='', login_out='login')
+        else:
+            self.redirect("/")
+
+class Signup(Handler):
+    def get(self):
+        self.render('signup.html', user='', login_out='login')
+        self.render('header.html', team_name='')
+
+    def post(self):
+        username = self.request.get('username')
+        email = self.request.get('email')
+        team = self.request.get("team")
+        #team_img = self.request.get("team_image")
+        email_error, team_error, uname_error = None, None, None
+
+
+        # user = User.by_email(email)
+        # if user:
+        #     user_error = "User account already exists"
+
+        if not email:
+            email_error = "Please use a valid email."
+
+        if not team:
+            team_error = "Please put a valid team name."
+
+        if not username:
+            uname_error = "Please enter a username."
+
+        if (email_error or team_error):
+            self.render("login.html", username=username, email=email, team=team,
+             uname_error=uname_error, team_error=team_error, email_error=email_error)
+            self.render('header.html', team_img='', login_out='login')
+        else:
+            #create a new user
+            create_user(username, email, team)
+            self.redirect("/")
+
+def create_user(name, email, team_name):
+    u = User.by_email(email)
+
+    print 'email here?  %s' % u.email
+
+    # check if user already exists.
+    if u:
+        #msg = 'User already registered.'
+        self.redirect('/')
+    else:
+        new_user = User(name=name, email=email, team=team_name)
+        new_user.put()
+        self.session['email'] = u.email
+        self.redirect('/')
+
+class Logout(Handler):
+    def get(self):
+        logout_url = 'http://localhost:8080' + users.create_logout_url('/')
+        #user = users.get_current_user()
+        print '##########  user %s' % user
+        self.render('login.html', user=user, login_out='login',
+            logout_url=logout_url)
+        self.render('header.html', user=user, team_name='')
 
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/sign', Guestbook),
-], debug=True)
+    ('/signup', Signup),
+    ('/login', Login),
+    ('/logout', Logout),
+], debug=True, config=config)
 # [END app]
