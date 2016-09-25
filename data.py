475dd585d94300e291aa39acc1d21c604173d487
svishwak4 <svishwak4@gmail.com>
2016-09-25 13:13:37 -0700

    Excel parsing implemented

--- data.py
+++ data.py
@@ -25,6 +25,7 @@ from models import Audit
 # [START waste_item]
 class WasteItem(ndb.Model):
     # ItemId = ndb.IntegerProperty()  # Represented by entity key, which is auto-generated after we store object to data store
+    EntryLineNum = ndb.IntegerProperty() # Shouldn't really be needed, but just in case
     AppraisalDate = ndb.DateProperty()
     InspectionType = ndb.StringProperty()
     LeadAppraiser = ndb.StringProperty()
@@ -39,13 +40,14 @@ class WasteItem(ndb.Model):
     PostalCode = ndb.StringProperty()
     BuildingType = ndb.StringProperty()
     NumberInhabitants = ndb.IntegerProperty()
-    PrimaryMaterial = ndb.StringProperty() # csv list
+    PrimaryMaterial = ndb.StringProperty() # TODO read this as csv list
     SecondaryMaterial = ndb.StringProperty() # TODO read this as csv list
-    TertQuatMaterial = ndb.StringProperty() # csv list
+    TertQuatMaterial = ndb.StringProperty() # TODO read this as csv list
     DisposalMethod = ndb.StringProperty()
     WasteKg = ndb.FloatProperty()
     RecycledKg = ndb.FloatProperty()
     ReusedKg = ndb.FloatProperty()
+    TotalKg = ndb.FloatProperty()
     KgInhabitant = ndb.FloatProperty()
     AdditionalComments = ndb.StringProperty()
     '''
@@ -99,12 +101,44 @@ def process_spreadsheet(blob_info):
     wb = xlrd.open_workbook(file_contents=blob_reader.read())
     sh = wb.sheet_by_index(0) #get the first worksheet in the workbook
     # the headers are currently on line 8 of the spreadsheet, fyi.
+    # So row[8] is the ninth row on the spreadsheet, where the actual data starts
     for rownum in range(8,sh.nrows):
     #for row in reader:
     #need to map all of the columns here - it is done by index of the column per ordering above
     #see the 'UploadPlaceholder as a simpler example, also included some code for getting data value from a spreadsheet
-        date, data, value = sh.row_values(rownum)
-        entry = WasteItem(date=datetime.date(1900, 1, 1) + datetime.timedelta(int(date)-2), data=data, value=int(value))
+        #date, data, value = sh.row_values(rownum)
+        #entry = WasteItem(date=datetime.date(1900, 1, 1) + datetime.timedelta(int(date)-2), data=data, value=int(value))
+        #entry.put()
+        # Columns A-Z
+        #for colnum in range(0,25):
+        line = int(sh.cell_value(rownum,0))
+        date = (xlrd.xldate.xldate_as_datetime(sh.cell_value(rownum,1),wb.datemode)).date()
+        ins_type = str(sh.cell_value(rownum,2))
+        appraiser = str(sh.cell_value(rownum,3))
+        ctry = str(sh.cell_value(rownum,4))
+        prov = str(sh.cell_value(rownum,5))
+        reg_dist = str(sh.cell_value(rownum,6))
+        city = str(sh.cell_value(rownum,7))
+        nebhood = str(sh.cell_value(rownum,8))
+        str_num = int(sh.cell_value(rownum,9))
+        str_nam = str(sh.cell_value(rownum,10))
+        apt_num = int(sh.cell_value(rownum,11))
+        poc_code = str(sh.cell_value(rownum,12))
+        bld_type = str(sh.cell_value(rownum,13))
+        sbd_type = str(sh.cell_value(rownum,14))
+        numb_inh = int(sh.cell_value(rownum,15))
+        prim_mat = str(sh.cell_value(rownum,16))
+        seco_mat = str(sh.cell_value(rownum,17))
+        terq_mat = str(sh.cell_value(rownum,18))
+        disp_mth = str(sh.cell_value(rownum,19))
+        waste_kg = float(sh.cell_value(rownum,20))
+        rcycd_kg = float(sh.cell_value(rownum,21))
+        rused_kg = float(sh.cell_value(rownum,22))
+        total_kg = float(sh.cell_value(rownum,23))
+        kg_inhab = float(sh.cell_value(rownum,24))
+        add_commnts = str(sh.cell_value(rownum,25))
+        
+        entry = WasteItem(EntryLineNum = line,AppraisalDate = date,InspectionType = ins_type,LeadAppraiser = appraiser,Country = ctry,Province = prov,RegionalDistrict = reg_dist,City_Town = city,Neighbourhood = nebhood,StreetNumber = str_num,StreetName = str_nam,ApartmentNumer = apt_num,PostalCode = poc_code,BuildingType = bld_type,BuildingSubType = sbd_type,NumberInhabitants = numb_inh,PrimaryMaterial = prim_mat,SecondaryMaterial = seco_mat,TertQuatMaterial = terq_mat,DisposalMethod = disp_mth,WasteKg = waste_kg,RecycledKg = rcycd_kg,ReusedKg = rused_kg,TotalKg = total_kg,KgInhabitant = kg_inhab,AdditionalComments = add_commnts)
         entry.put()
-
+        
 # [END spreadsheet_import]
